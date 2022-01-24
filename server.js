b0bd1d4286173780e7195a5b758707cbb596adf8
alvaro-freire <alvaro.freirea@udc.es>
2022-01-25 00:01:42 +0100

    add <username> and manage inputs

--- server.js
+++ server.js
@@ -3,14 +3,34 @@ const port = 8080
 
 const server = net.createServer((socket) => {
     console.log('opened connection', socket.remoteAddress, socket.remotePort)
+    username = socket.remotePort + ' '
 
     socket.on('data', (data) => {
-        console.log("wrote this: " + data)
+        const first = String(data).split(' ')[0]
+        message = String(data).replace(first + " ", '').trim()
+
+        if (first == 'SEND') {
+            socket.write('RECEIVE ' + username + ' ' + message + '\n')
+        } else if (first == 'USERNAME') {
+            if (message.indexOf(' ') !== -1) {
+                socket.write('ERROR invalid username\n')
+            } else {
+                username = message
+            }
+        } else {
+            if (String(data) == 'USERNAME\n') {
+                socket.write('USERNAME -> ' + username + '\n')
+            } else {
+                socket.write('ERROR invalid command\n')
+            }
+        }
     })
 
     socket.on('close', () => {
         console.log('closed connection', socket.remoteAddress, socket.remotePort)
     })
+
+
 
 })
 
