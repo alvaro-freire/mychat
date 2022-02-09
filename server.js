3871602d5ec5fb54729c49866c2d07604d6194d8
alvaro-freire <alvaro.freirea@udc.es>
2022-02-09 20:39:50 +0100

    fixed username

--- server.js
+++ server.js
@@ -5,28 +5,27 @@ const clientList = []
 const server = net.createServer((socket) => {
     console.log('opened connection', socket.remoteAddress, socket.remotePort)
     clientList.push(socket)
-    username = socket.remotePort + ' '
+    socket.username = socket.remotePort + ' '
 
     socket.on('data', (data) => {
         const first = String(data).split(' ')[0]
         message = String(data).replace(first + " ", '').trim()
 
         if (first == 'SEND') {
-            socket.write('RECEIVE port:' + username + '-> ' + message + '\n')
-
             clientList.forEach((client) => {
-                socket.write('client port -> ' + client.remotePort + '\n')
+                if (client != socket) {
+                    client.write('RECEIVE ' + socket.username + message + '\n')
+                }
             })
-
         } else if (first == 'USERNAME') {
             if (message.indexOf(' ') !== -1) {
                 socket.write('ERROR invalid username\n')
             } else {
-                username = message
+                socket.username = message + ' '
             }
         } else {
             if (String(data) == 'USERNAME\n') {
-                socket.write('USERNAME -> ' + username + '\n')
+                socket.write('USERNAME -> ' + socket.username + '\n')
             } else {
                 socket.write('ERROR invalid command\n')
             }
